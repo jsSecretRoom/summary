cd0970b64cfdd7ee4576089b30421114bdd07d9c
Ruslan <g.ruslan.rus@gmail.com>
2023-08-24 13:47:17 +0300

    dayry1

--- src/components/Diary/DiaryComponnt.js
+++ src/components/Diary/DiaryComponnt.js
@@ -1,4 +1,3 @@
-import './diary.scss';
 import React, { useState, useEffect } from 'react';
 import AuthPopup from '../AuthPopup/AuthPopup';
 import Animations from '../Animations/Animations';
@@ -11,16 +10,24 @@ function Diary() {
     const [diaryText, setDiaryText] = useState('');
     const [diaryEntries, setDiaryEntries] = useState([]);
 
+    const [entries, setEntries] = useState([]); // Добавленное состояние для хранения записей
+
     useEffect(() => {
         Animations('.diary-group');
         Animations('.diary-body');
         Animations('.diary-footer');
-    });
+
+        // Запрос на получение записей при монтировании компонента
+        fetch('https://summary-iota-indol.vercel.app/api/diary') // Замените на URL вашего сервера на Vercel
+            .then(response => response.json())
+            .then(data => setEntries(data))
+            .catch(error => console.error('Error fetching diary entries:', error));
+    }, []);
 
     useEffect(() => {
         const storedEntries = localStorage.getItem('diaryEntries');
         if (storedEntries) {
-        setDiaryEntries(JSON.parse(storedEntries));
+            setDiaryEntries(JSON.parse(storedEntries));
         }
     }, []);
 
@@ -34,8 +41,8 @@ function Diary() {
 
     const handleEnterKey = (e) => {
         if (e.key === 'Enter' && diaryText.trim() !== '') {
-        setDiaryEntries([...diaryEntries, diaryText]);
-        setDiaryText('');
+            setDiaryEntries([...diaryEntries, diaryText]);
+            setDiaryText('');
         }
     };
 
@@ -67,25 +74,25 @@ function Diary() {
             
             {isAuthenticated && (
                 <div className='diary-body'>
-                <input
-                    type="text"
-                    name="diary-text"
-                    id="diary-text"
-                    placeholder='my-diary-text'
-                    value={diaryText}
-                    onChange={handleInputChange}
-                    onKeyDown={handleEnterKey}
-                />
+                    <input
+                        type="text"
+                        name="diary-text"
+                        id="diary-text"
+                        placeholder='my-diary-text'
+                        value={diaryText}
+                        onChange={handleInputChange}
+                        onKeyDown={handleEnterKey}
+                    />
                 </div>
             )}
             <div className='diary-footer'>
-                {diaryEntries.map((entry, index) => (
-                <div className='list' key={index}>
-                    <p>{entry}</p>
-                    <button onClick={() => handleDeleteEntry(index)}>
-                    <img src={Trach} alt="Trach" />
-                    </button>
-                </div>
+                {entries.map((entry, index) => (
+                    <div className='list' key={index}>
+                        <p>{entry.text}</p>
+                        <button onClick={() => handleDeleteEntry(index)}>
+                            <img src={Trach} alt="Trach" />
+                        </button>
+                    </div>
                 ))}
             </div>
         </section>
