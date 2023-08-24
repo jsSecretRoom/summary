439067f61b1da1333a0fe1f3965088c6998bc4cc
Ruslan <g.ruslan.rus@gmail.com>
2023-08-24 14:06:39 +0300

    dayry-problem

--- src/components/Diary/DiaryComponnt.js
+++ src/components/Diary/DiaryComponnt.js
@@ -3,20 +3,21 @@ import AuthPopup from '../AuthPopup/AuthPopup';
 import Animations from '../Animations/Animations';
 import Trach from '../../img/Trash.svg';
 import Atom from '../../img/Atom.svg';
+import './diary.scss'
 
 function Diary() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [showAuthPopup, setShowAuthPopup] = useState(false);
     const [diaryText, setDiaryText] = useState('');
-    const [diaryEntries, setDiaryEntries] = useState([]);
-
-    const [entries, setEntries] = useState([]); // Добавленное состояние для хранения записей
+    const [entries, setEntries] = useState([]); // Здесь храним записи
 
     useEffect(() => {
         Animations('.diary-group');
         Animations('.diary-body');
         Animations('.diary-footer');
+    }, []);
 
+    useEffect(() => {
         // Запрос на получение записей при монтировании компонента
         fetch('https://summary-iota-indol.vercel.app/api/diary') // Замените на URL вашего сервера на Vercel
             .then(response => response.json())
@@ -25,15 +26,8 @@ function Diary() {
     }, []);
 
     useEffect(() => {
-        const storedEntries = localStorage.getItem('diaryEntries');
-        if (storedEntries) {
-            setDiaryEntries(JSON.parse(storedEntries));
-        }
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem('diaryEntries', JSON.stringify(diaryEntries));
-    }, [diaryEntries]);
+        localStorage.setItem('diaryEntries', JSON.stringify(entries));
+    }, [entries]);
 
     const handleInputChange = (e) => {
         setDiaryText(e.target.value);
@@ -41,14 +35,15 @@ function Diary() {
 
     const handleEnterKey = (e) => {
         if (e.key === 'Enter' && diaryText.trim() !== '') {
-            setDiaryEntries([...diaryEntries, diaryText]);
+            const newEntry = { text: diaryText };
+            setEntries([...entries, newEntry]); // Добавляем новую запись в entries
             setDiaryText('');
         }
     };
 
     const handleDeleteEntry = (index) => {
-        const newEntries = diaryEntries.filter((_, i) => i !== index);
-        setDiaryEntries(newEntries);
+        const newEntries = entries.filter((_, i) => i !== index);
+        setEntries(newEntries);
     };
 
     const handleAuthenticate = () => {
