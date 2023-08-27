b89d7eea4f3236ea59383af904f2f6b16ba6dd89
Ruslan <g.ruslan.rus@gmail.com>
2023-08-27 13:13:04 +0300

    V.03

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const db = require('./your-database-module'); // Подключение к базе данных
+const db = require('./db'); // Подключение к базе данных
 
 app.use(bodyParser.json());
 
 app.post('/api/save-entry', async (req, res) => {
   try {
-    const { entryText } = req.body; // Получение данных от клиента
+    const { entryText } = req.body;
     await db.query('INSERT INTO diary_entries (entry_text) VALUES ($1)', [entryText]);
     res.status(201).send('Entry saved successfully');
   } catch (error) {
@@ -16,7 +16,9 @@ app.post('/api/save-entry', async (req, res) => {
   }
 });
 
-// Запуск сервера на порте 5000
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+// Другие маршруты и настройки сервера...
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
