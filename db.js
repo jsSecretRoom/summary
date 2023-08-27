a7952c5aca94b1fc6cfa2936ea35ad6dafdae06d
Ruslan <g.ruslan.rus@gmail.com>
2023-08-27 15:14:14 +0300

    v0

--- db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: 'psql "postgres://default:Nfjzd7ohZ3Wl@ep-weathered-surf-93041641.eu-central-1.postgres.vercel-storage.com:5432/verceldb"',
-});
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
--- src/components/Diary/DiaryComponnt.js
+++ src/components/Diary/DiaryComponnt.js
@@ -6,12 +6,25 @@ import Animations from '../Animations/Animations';
 import Trach from '../../img/Trash.svg';
 import Atom from '../../img/Atom.svg';
 
+const initialDiaryEntries = [
+    "14/05/2022 - Started learning frontend.",
+    "14/07/2022 - Learned basic HTML AND CSS ",
+    "14/08/2022 - I practiced cross-browser layout, consolidated my knowledge in practice",
+    "15/08/2022 - Started reading the basic java script",
+    "01/09/2022 - At the same time, having read English",
+    "01/01/2023 - Tested js base, practiced codewars, read documentation",
+    "02/10/2023 - Starting js & React course on udemy",
+    "01/04/2023 - Actively hacking js and practicing it in my projects, improving my knowledge of the design and logic of sites",
+    "04/15/2023 - Actively read the React library",
+    "08/15/2023 - Having created this project, I've enriched the project and my portfolio",
+    "08/27/2023 - I will study additional libraries and learn advanced practices",
+];
 
 function Diary() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [showAuthPopup, setShowAuthPopup] = useState(false);
     const [diaryText, setDiaryText] = useState('');
-    const [diaryEntries, setDiaryEntries] = useState([]);
+    const [diaryEntries, setDiaryEntries] = useState(initialDiaryEntries);
 
     useEffect(() => {
         Animations('.diary-group');
